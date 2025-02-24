9c5a6e9d3330acfec781989e3ab1c80b1faed9b8
Lind7Wake <88340801+Lucas-Dominguez0@users.noreply.github.com>
2025-02-24 07:35:30 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+let numeroSecreto = gerarNumero();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'ACERTOU')
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `parabens voce acertou com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', mensagemTentativas)
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else {
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p', 'o numero secreto e menor')
+        }  else{
+            exibirTextoNaTela('p', 'o numero secreto e maior')
+        }
+        tentativas++
+        limparCampo()
+    }
+   
+}
+
+function gerarNumero() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo(){
+    chute = document.querySelector('input')
+    chute.value = ''
+}
+
